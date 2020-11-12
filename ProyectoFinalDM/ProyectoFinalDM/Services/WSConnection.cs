10a522a30570c51f9db00c09b12ab07dff976ae3
dach1996 <dach19962012@gmail.com>
2020-11-12 10:28:05 -0500

    Se implementa Clase abstracta WSConecction

---
+++ ProyectoFinalDM/ProyectoFinalDM/Services/WSConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+
+namespace ProyectoFinalDM.Services
+{
+    public abstract class WSConnection<T>
+    {
+
+        protected string Url = "http://192.168.100.29/WebService/Api/";
+        protected HttpClient httpClient = new HttpClient();
+        protected ObservableCollection<T> consulta;
+
+    }
+}
--- ProyectoFinalDM/ProyectoFinalDM/Services/WSImplements/UsuarioServiceImplWS.cs
+++ ProyectoFinalDM/ProyectoFinalDM/Services/WSImplements/UsuarioServiceImplWS.cs
@@ -11,34 +11,29 @@ using System.Threading.Tasks;
 
 namespace ProyectoFinalDM.Services.WSImplements
 {
-   public class UsuarioServiceImplWS : IUsuariosService
+   public class UsuarioServiceImplWS : WSConnection<UsuarioModel>, IUsuariosService
     {
-        private string Url;
-        private HttpClient httpClient;
-        private ObservableCollection<UsuarioModel> usuarios;
-
-        
         public UsuarioServiceImplWS()
         {
-            httpClient = new HttpClient();
-            Url = "http://192.168.100.29/WebService/Api/usuario.php";
-        } 
+            base.httpClient = new HttpClient();
+            base.Url = base.Url + "usuario.php";
+        }
         private async Task consultarJson()
         { 
                 var consultaSerializada = await httpClient.GetStringAsync(Url);
                 var consultaDeserializada = JsonConvert.DeserializeObject<List<UsuarioModel>>(consultaSerializada);
-                usuarios = new ObservableCollection<UsuarioModel>(consultaDeserializada);
+                consulta = new ObservableCollection<UsuarioModel>(consultaDeserializada);
         }
         public async Task<UsuarioModel> buscarUsuario(string username, string password)
         {
             await this.consultarJson();
-            return usuarios.FirstOrDefault(u => u.UsernameUsuario == username && u.PasswordUsuario == password);
+            return consulta.FirstOrDefault(u => u.UsernameUsuario == username && u.PasswordUsuario == password);
         }
 
         public ObservableCollection<UsuarioModel> listarUsuarios()
         {
-            this.consultarJson();
-            return usuarios;
+
+            return consulta;
         }
 
  
